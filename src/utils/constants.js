bf18e2bc949b368c10254f924d9a2f420e565552
ramiawed <48353547+ramiawed@users.noreply.github.com>
2022-02-02 23:21:24 +0200

    update 2-2-2022

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -133,7 +133,7 @@ export const onKeyPressForNumberInput = (event) => {
   return event.charCode >= 48 && event.charCode <= 57;
 };
 
-// export const BASEURL = "https://salty-brook-65681.herokuapp.com/api/v1";
-export const BASEURL = "http://localhost:8000/api/v1";
-// export const SERVER_URL = "https://salty-brook-65681.herokuapp.com";
-export const SERVER_URL = "http://localhost:8000";
+export const BASEURL = "https://salty-brook-65681.herokuapp.com/api/v1";
+// export const BASEURL = "http://localhost:8000/api/v1";
+export const SERVER_URL = "https://salty-brook-65681.herokuapp.com";
+// export const SERVER_URL = "http://localhost:8000";

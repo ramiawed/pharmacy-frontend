960128310e5a07273269763389e6d0cb3886cb35
ramiawed <48353547+ramiawed@users.noreply.github.com>
2022-02-24 03:44:17 +0200

    update 24-2-2022

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -156,7 +156,7 @@ export const onKeyPressForNumberInput = (event) => {
 
 // export const BASEURL = "https://salty-brook-65681.herokuapp.com/api/v1";
 // export const BASEURL = "http://localhost:8000/api/v1";
-export const BASEURL = "http://67.205.165.65/api/api/v1";
+export const BASEURL = "http://67.205.165.65/api/v1";
 // export const SERVER_URL = "https://salty-brook-65681.herokuapp.com";
 // export const SERVER_URL = "http://localhost:8000";
 export const SERVER_URL = "http://67.205.165.65";
